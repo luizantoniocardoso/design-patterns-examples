3ef99d69519efa27be118e5f39ca89ecf1a59768
Luiz Antonio De Souza Cardoso <luiz.129922@alunosatc.edu.br>
2025-09-17 19:29:12 -0300

    termiando a prova

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,75 @@
-console.log("Hello, World! 001");
+class Lanche {
+    constructor(pao, carne, queijo, salada, molho) {
+      this.pao = pao;
+      this.carne = carne;
+      this.queijo = queijo;
+      this.salada = salada;
+      this.molho = molho;
+    }
+  
+    show() {
+      console.log("Lanche:", {
+        pao: this.pao,
+        carne: this.carne,
+        queijo: this.queijo,
+        salada: this.salada,
+        molho: this.molho,
+      });
+    }
+  }
+  
+  class LancheBuilder {
+    constructor() {
+      this.pao = false;
+      this.carne = false;
+      this.queijo = false;
+      this.salada = false;
+      this.molho = false;
+    }
+  
+    comPao() {
+      this.pao = true;
+      return this;
+    }
+  
+    comCarne() {
+      this.carne = true;
+      return this;
+    }
+  
+    comQueijo() {
+      this.queijo = true;
+      return this;
+    }
+  
+    comSalada() {
+      this.salada = true;
+      return this;
+    }
+  
+    comMolho() {
+      this.molho = true;
+      return this;
+    }
+  
+    build() {
+      return new Lanche(this.pao, this.carne, this.queijo, this.salada, this.molho);
+    }
+  }
+  
+  const lancheSimples = new LancheBuilder()
+    .comPao()
+    .comCarne()
+    .build();
+  
+  const lancheCompleto = new LancheBuilder()
+    .comPao()
+    .comCarne()
+    .comQueijo()
+    .comSalada()
+    .comMolho()
+    .build();
+  
+  lancheSimples.show();
+  lancheCompleto.show();
+  
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,30 @@
-console.log("Hello, World! 002");
+class Config {
+    constructor() {
+      if (Config.instance) {
+        return Config.instance; 
+      }
+      
+      this.settings = {};
+      Config.instance = this;  
+    }
+    
+    setConfig(key, value) {
+      this.settings[key] = value;
+    }
+    
+    getConfig(key) {
+      return this.settings[key];
+    }
+  }
+  
+  const c1 = new Config();
+  c1.setConfig("lang", "pt-BR");
+  
+  const c2 = new Config();
+  console.log(c1)
+  console.log(c2)
+  console.log(c2.getConfig("lang")); 
+  
+  console.log(c1 === c2); 
+
+  
